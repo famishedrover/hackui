22252cafade4660450129a77d2b44c7b1e4a851e
sagarpreet chadha <chadha.sagarpreet97@gmail.com>
2017-02-11 04:58:18 +0530

    causes implemented

---
+++ app/src/main/java/com/example/android/wildcards/CausesActivity.java
@@ -0,0 +1,13 @@
+package com.example.android.wildcards;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class CausesActivity extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_causes);
+    }
+}
